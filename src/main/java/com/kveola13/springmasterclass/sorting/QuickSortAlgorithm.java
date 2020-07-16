e657ed656a27c347112faaff57d95c16c776ef69
olakv <kveola13@student.westerdals.no>
2020-07-16 08:23:42 +0200

    Sort implementations

---
+++ src/main/java/com/kveola13/springmasterclass/sorting/QuickSortAlgorithm.java
@@ -0,0 +1,10 @@
+package com.kveola13.springmasterclass.sorting;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuickSortAlgorithm implements SortAlgorithm {
+    public int[] sort(int[] numbers) {
+        return numbers;
+    }
+}
