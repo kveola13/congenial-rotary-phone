cfe04255cdaa79af0ffdf98da69775720d543947
olakv <kveola13@student.westerdals.no>
2020-07-17 08:19:07 +0200

    Added tests

---
+++ src/test/java/com/kveola13/springmasterclass/SpringMasterclassApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kveola13.springmasterclass;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+class SpringMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
