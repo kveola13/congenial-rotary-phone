555954ad8e6d69fe75cec3256f9cb63528cd3d8d
olakv <kveola13@student.westerdals.no>
2020-07-19 00:09:34 +0200

    added xml dao

---
+++ src/main/java/com/kveola13/springmasterclass/xml/XMLPersonDAO.java
@@ -0,0 +1,13 @@
+package com.kveola13.springmasterclass.xml;
+
+public class XMLPersonDAO {
+    XMLJdbcConnection xmlJdbcConnection;
+
+    public XMLJdbcConnection getXmlJdbcConnection() {
+        return xmlJdbcConnection;
+    }
+
+    public void setXmlJdbcConnection(XMLJdbcConnection xmlJdbcConnection) {
+        this.xmlJdbcConnection = xmlJdbcConnection;
+    }
+}
