f8f9435b8eded07b2d867864e9d381ed44e1656a
olakv <kveola13@student.westerdals.no>
2020-07-19 00:09:29 +0200

    added xml jdbc

---
+++ src/main/java/com/kveola13/springmasterclass/xml/XMLJdbcConnection.java
@@ -0,0 +1,7 @@
+package com.kveola13.springmasterclass.xml;
+
+public class XMLJdbcConnection {
+    public XMLJdbcConnection() {
+        System.out.println("Connection established with XML JDBC!");
+    }
+}
